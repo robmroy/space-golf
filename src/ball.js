563b1d49ddcb238374e694b2708f124a86399418
robmroy <robmyroy@gmail.com>
2019-07-19 13:47:13 -0700

    get the ball moving

--- src/ball.js
+++ src/ball.js
@@ -1,18 +1,28 @@
 
 class Ball {
-    constructor(game, x = 0, y = 0, radius = 5, color = "white"){
+    constructor(game, x = 0, y = 0, radius = 5, color = "white", vx=0, vy=0){
         this.x = x;
         this.y = y;
         this.radius = radius;
-        console.log('constructing ball');
+        this.color = color;
+        this.draw= this.draw.bind(this);
+        this.move = this.move.bind(this);
+        this.vx = vx;
+        this.vy = vy;
     }
 
+    move(){
+        console.log(`ball.x=${this.x}`);
+        console.log(`ball.y=${this.y}`);
+        this.x += this.vx;
+        this.y += this.vy;
+    }
     draw(ctx ) {
         ctx.fillStyle = this.color;
       
         ctx.beginPath();
         ctx.arc(
-          this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
+          this.x, this.y, this.radius, 0, 2 * Math.PI, true
         );
         ctx.fill();
       };
--- src/game.js
+++ src/game.js
@@ -7,9 +7,9 @@ class Game {
     constructor(level) {
         this.canvas = document.getElementById("game-canvas");
         this.ctx = this.canvas.getContext("2d");
-        console.dir(this.canvas.ctx);
 
         this.ball = new Ball(this, 50, 50);
+        this.ball.vx = 1;
         this.draw = this.draw.bind(this);
         // this.planets = new Planets(level);
         // this.launchpad = new LaunchPad(level);
@@ -25,11 +25,10 @@ class Game {
     }
 
     moveObjects() {
-
+        this.ball.move();
     }
     animate(time) {
         const timeDelta = time - this.lastTime;
-        console.log(`animate!`);
         this.step(timeDelta);
         this.draw();
         this.lastTime = time;
@@ -38,9 +37,12 @@ class Game {
         requestAnimationFrame(this.animate.bind(this));
     }
     draw() {
-        let game= this;
-        console.log(`game ctx: ${game.ctx}`);
-        this.ball.draw(game.ctx);
+        let ctx=this.ctx;
+        ctx.width = 1200;
+        ctx.height = 600;
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, 1000, 600);
+        this.ball.draw(this.ctx);
     }
 
 }
