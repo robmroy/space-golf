6379b6c3d36e4e3a59ecacbd0b6a1581a1b29b74
Rob <robmyroy@gmail.com>
2019-11-12 07:33:01 -0800

    add to obstacle on level9

--- src/levels/level9.js
+++ src/levels/level9.js
@@ -15,10 +15,12 @@ class Level9 {
             this.currentPlanet,
             new StickyPlanet(game, 1120, 1200, 750, "orange", .005)            
         ]
+        const circle = polyFromFunction(game, t=> 1120 + 1000 * (t/3.96) * Math.cos(t), 
+        t => 1200 + 1000 * (t/3.96) * Math.sin(t), 3, 3.96, Math.PI/60);
         const ellipse2 = polyFromFunction(game,
-            t => 50 + 400* Math.cos(t), t => 400 - 250* Math.sin(t), -.9,    Math.PI, Math.PI/60);
-        this.obstacles = [
-        ].concat(ellipse2);
+            t => 50 + 400* Math.cos(t), t => 400 - 250* Math.sin(t), -.43,    Math.PI, Math.PI/60);
+        this.obstacles = [ 
+        ].concat(circle).concat(ellipse2);
         let sqrt = 1/Math.sqrt(2);
         this.hole = new Hole(game, 30, 430,  [0, -1], 480);
         this.viewportStyle = 'centered';
